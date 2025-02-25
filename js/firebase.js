27dcfb524b3412aa98a4ecee3ba0d1d6c61c698b
Ibon <castroibon216@gmail.com>
2025-02-25 22:07:20 +0100

    Cambios

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXqlo1NT5GFKPCu8yleV5AaRPgZpvLqHc",
+  authDomain: "isaca-rooted.firebaseapp.com",
+  projectId: "isaca-rooted",
+  storageBucket: "isaca-rooted.firebasestorage.app",
+  messagingSenderId: "102143926859",
+  appId: "1:102143926859:web:71a24a515c485a104763ce",
+  measurementId: "G-985BD6PMFX"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc, getDocs, deleteDoc, doc };
